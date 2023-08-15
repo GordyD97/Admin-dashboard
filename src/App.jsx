1d2f7256575bcd8c2d5e9e9db2d77669de05d589
Gordon D <gordon.draine97@yahoo.com>
2023-08-14 21:01:26 -0400

    setting theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-
-
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, colorMode] = useMode();
 
   return (
-    <>
-      
-    </>
-  )
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="context"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- src/theme.js
+++ src/theme.js
@@ -151,8 +151,73 @@ export const themeSettings = (mode) => {
                 }
                 // stopped at @26:48 in video. 
 
-            }
-            )
+            } : {
+                    primary: {
+                        main: colors.primary[100],
+                    },
+                    secondary: {
+                        main: colors.greenAccent[500],
+                    },
+                    netural: {
+                        dark: colors.grey[700],
+                        main: colors.grey[500],
+                        light: colors.grey[100],
+                    },
+                    background: {
+                        default: "#fcfcfc",
+                    }
+            })
+        },
+        typography: {
+            fontFamily: ["Source sans pro", "sans-serif"].join(","),
+            fontSize: 12,
+             h1: {
+                 fontFamily: ["Source sans pro", "sans-serif"].join(","),
+                 fontSize: 40,
+            },
+             h2: {
+                 fontFamily: ["Source sans pro", "sans-serif"].join(","),
+                 fontSize: 32,
+            },
+             h3: {
+                 fontFamily: ["Source sans pro", "sans-serif"].join(","),
+                 fontSize: 24,
+            },
+             h4: {
+                 fontFamily: ["Source sans pro", "sans-serif"].join(","),
+                 fontSize: 20,
+            },
+             h5: {
+                 fontFamily: ["Source sans pro", "sans-serif"].join(","),
+                 fontSize: 16,
+            },
+             h6: {
+                 fontFamily: ["Source sans pro", "sans-serif"].join(","),
+                 fontSize: 14,
+            },
+             
         }
-    }
+    };
+};
+
+export const ColorModeContext = createContext({
+    toggleColorMode: () => {}
+});
+
+export const useMode = () => {
+    const [mode, setMode] = useState("dark");
+
+    const colorMode = useMemo(
+        () => ({
+            toggleColorMode: () =>
+            setMode((prev) => (prev === "light" ? "dark" : "light"))
+        }),
+        []
+    );
+
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+
+    return [theme, colorMode];
 }
+
+// the function above easy access of the controller for giving condition on whether the theme will be either dark or light, 
