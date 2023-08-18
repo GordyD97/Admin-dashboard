552270fe4a832f225ebad4f2b7cd63cae80850eb
Gordon D <gordon.draine97@yahoo.com>
2023-08-18 00:25:33 -0400

    resolving sidebar syntax issue

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ import Dashboard from "./scenes/dashboard";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
+
+
 // import Calendar from "./scenes/calendar/calendar";
 
 function App() {
