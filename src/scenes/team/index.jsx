b8c83d26f27cbb39b00e99f204177bf6e116abb9
Gordon D <gordon.draine97@yahoo.com>
2023-08-20 21:04:27 -0400

    finishing up team page with one visual error

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -8,7 +8,7 @@ import  SecurityOutlinedIcon  from "@mui/icons-material/SecurityOutlined";
 import Header from "../../components/Header";
 
 import React from 'react'
-import { L } from "@fullcalendar/list/internal-common";
+
 
 const Team = () => {
 const theme = useTheme();
@@ -16,27 +16,78 @@ const colors = tokens(theme.palette.mode);
 
 const columns = [
     { field : "id", headerName: "ID" }, 
-{ field : "name", headerName: "Name", flex: 1, cellClassName: "name-column--cell"}
-{ field : "age", headerName: "Age", type: "Number", headerAlign: "left", cellClassName: "name-column--cell"}
+{ field : "name", headerName: "Name", flex: 1, cellClassName: "name-column--cell",},
+{ field : "age", headerName: "Age", type: "Number", headerAlign: "left", align: "left", },
+{ field : "phone", headerName: "Phone Number", flex: 1, },
+{ field : "email", headerName: "Email", flex: 1, },
+{ field : "access", headerName: "Access Level", flex: 1, renderCell: ({row: { access }}) => {
+    return (
+        <Box
+        width="60%"
+        m="0 auto"
+        p="5px"
+        display="flex"
+        justifyContent="center"
+        backgroundColor= {
+            access === "admin"
+            ? colors.greenAccent[600]
+            : colors.greenAccent[700]
+        }
+        borderRadius="4px"
+        >
+            {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
+            {access === "manager" && <SecurityOutlinedIcon />}
+            {access === "user" && <LockOpenOutlinedIcon />}
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+                {access}
+            </Typography>
+        </Box>
+    )
+}, }
 ];
 
+// stopped at 1:35:20
+
 return (
-    <Box>
-        <Header title="TEAM" subtitle="Managing the Team Members" />
-        <Box>
-            <DataGrid
-            rows={mockDataTeam}
-            columns={columns}
-            />
-        </Box>
+  <Box m="20px">
+    <Header title="TEAM" subtitle="Managing the Team Members" />
+    <Box
+      m="40px 0 0 0"
+      height="75vh"
+      sx={{
+        "& .MuiDataGrid-root": {
+          border: "none",
+        },
+        "& .MuiDataGrid-cell": {
+          borderBottom: "none",
+        },
+        "& .name-column--cell": {
+          color: colors.greenAccent[300],
+        },
+        "& .MuiDataGrid-columnHeaders": {
+          backgroundColor: colors.blueAccent[700],
+          borderBottom: "none",
+        },
+        "& .MuiDataGrid-virtualScroller": {
+          backgroundColor: colors.primary[400],
+        },
+        "& .MuiDataGrid-footerContainer": {
+          borderTop: "none",
+          backgroundColor: colors.blueAccent[700],
+        },
+        "& .MuiCheckbox-root": {
+          color: `${colors.greenAccent[200]} !important`,
+        },
+      }}
+    >
+      <DataGrid rows={mockDataTeam} columns={columns} />
     </Box>
-)
+  </Box>
+);
 
 
 
-  return (
-    <div>index</div>
-  )
-}
+ 
+};
 
 export default Team;
