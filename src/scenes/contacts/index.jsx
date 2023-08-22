53a7dc3d26283333f7908f1f3d6ddae73e30e3a2
Gordon D <gordon.draine97@yahoo.com>
2023-08-22 08:55:07 -0400

    adding contacts page

---
+++ src/scenes/contacts/index.jsx
@@ -0,0 +1,96 @@
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { GridToolbarQuickFilter } from "@mui/x-data-grid/components";
+import { tokens } from "../../theme";
+import { mockDataContacts } from "../../data/mockData";
+
+import Header from "../../components/Header";
+import { useTheme } from "@emotion/react";
+
+import React from "react";
+
+const Contacts = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  // columns will represent keys and properties on what we are using.
+  const columns = [
+    { field: "id", headerName: "ID", flex: 0.5 },
+    { field: "registrarId", headerName: "Registrar ID"},
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "Number",
+      headerAlign: "left",
+      align: "left",
+    },
+    { field: "phone", headerName: "Phone Number", flex: 1 },
+    { field: "email", headerName: "Email", flex: 1 },
+    { field: "address", headerName: "Address", flex: 1 },
+    { field: "city", headerName: "City", flex: 1 },
+    { field: "zipCode", headerName: "ZipCode", flex: 1 },
+    
+  ];
+
+  // stopped at 1:35:20
+  // find a fix for the size of the data grid.
+  // hard coded a width for the data grid since it didint work.
+  return (
+    <Box m="20px">
+      <Header
+        title="Contacts"
+        subtitle="List of contacts for future refrencce "
+      />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+            width: "850px",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+          "& MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          rows={mockDataContacts}
+          columns={columns}
+        //   components={{ Toolbar: GridToolbar }}
+          components={{toolbar: GridToolbar}}
+        //   components is depreicated cannot include tool settings.
+        // stopped at 1:52:30
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Contacts;
