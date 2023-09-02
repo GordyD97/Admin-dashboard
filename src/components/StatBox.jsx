4d9397d2809fb441f5dc5a7c253ccb2ffa9a7c0e
Gordon D <gordon.draine97@yahoo.com>
2023-09-01 22:37:17 -0400

    fixing stat box box syntax

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -13,17 +13,19 @@ const StatBox = ({ title, subtitle, icon, progress, increase}) => {
   return (
     <Box width="100%" m="0 30px">
       <Box display="flex" justifyContent="space-between">
-        {icon}
-        <Typography
-          variant="h4"
-          fontWeight="bold"
-          sx={{ color: colors.grey[100] }}
-        >
-          {title}
-        </Typography>
-      </Box>
-      <Box>
-        <ProgressCircle progress={progress} />
+        <Box>
+          {icon}
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            sx={{ color: colors.grey[100] }}
+          >
+            {title}
+          </Typography>
+        </Box>
+        <Box>
+          <ProgressCircle progress={progress} />
+        </Box>
       </Box>
       <Box display="flex" justifyContent="space-between">
         <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
@@ -31,7 +33,7 @@ const StatBox = ({ title, subtitle, icon, progress, increase}) => {
         </Typography>
         <Typography
           variant="h5"
-         fontStyle="italic"
+          fontStyle="italic"
           sx={{ color: colors.greenAccent[600] }}
         >
           {increase}
